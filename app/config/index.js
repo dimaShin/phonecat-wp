91fe4ece452117706d650fcf38c18d57072352df
Anton Kalmykov <a.kalmykov@outlook.com>
2016-03-29 15:00:44 +0300

    add config

---
+++ app/config/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+export class Config {
+
+  constructor($routeProvider) {
+    return $routeProvider.
+      when('/phones', {
+        template: '<sidebar></sidebar><phonelist></phonelist>'
+      }).
+      when('/phones/:phoneId', {
+        template: '<phonedetail></phonedetail>',
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }
+}
